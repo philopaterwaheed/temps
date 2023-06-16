10df9f8b872da865f1033750a3ea79950db27282
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-06-16 23:17:36 +0300

    Update is_prime.cpp

--- is_prime.cpp
+++ is_prime.cpp
@@ -1,7 +1,11 @@
-   bool is_prime (int n )
+    bool is_prime (int n )
    {
+    if (n == 2 )
+        return true;
+    if ( n == 1 )
+        return false ; 
    for (int i =2 ; i * i < (n) ; i ++ )
        if (n % i == 0 ) 
-          return true;
-      return false;
+          return false;
+      return true;
       }
