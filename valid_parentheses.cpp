5160f4b152546a971cd14b3fe517b41654fdd293
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2024-01-29 02:56:23 +0200

    made it cpp

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -1,14 +1,14 @@
-public boolean isValid(String s) {
-         Stack<Character> stack = new Stack<Character>(); 
-        for (char c : s.toCharArray()) { 
+bool isvalid(std::string s) {
+	std ::stack<char> stack ;  
+        for (char c : s) { 
             if (c == '(') 
-                stack.push(')'); 
+                stack.push (')'); 
             else if (c == '{') 
                 stack.push('}'); 
             else if (c == '[') 
                 stack.push(']'); 
-            else if (stack.isEmpty() || stack.pop() != c)
+            else if (stack.empty() || stack.top() != c)
                 return false;
         }
-        return stack.isEmpty();
+        return stack.empty();
     }
